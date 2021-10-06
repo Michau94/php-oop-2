d69e1ec7d54ffa527db5659d042514eb2f59c709
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-10-06 15:30:25 +0200

    added user Class * discount function

---
+++ User.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+class User
+{
+
+    protected $name;
+    protected $lastName;
+    protected $email;
+    protected $discount;
+    protected $level;
+
+    public function __construct($_name, $_lastName, $_email, $_level = 1)
+    {
+
+        $this->name = $_name;
+        $this->lastName = $_lastName;
+        $this->email = $_email;
+        $this->level = $_level;
+    }
+
+    protected function customerInfo()
+    {
+        return "Nome: $this->name Cognome: $this->lastName";
+    }
+
+    public function  getUserName()
+    {
+
+        return $this->customerInfo();
+    }
+
+    protected function setDiscount()
+    {
+        if ($this->level > 1) {
+            $this->discount = 10;
+        } else if ($this->level > 3) {
+            $this->discount = 25;
+        } else {
+            $this->discount = 0;
+        }
+    }
+
+    public function getDiscount()
+    {
+        $this->setDiscount();
+        return " Il tuo sconto è : $this->discount";
+    }
+
+
+    protected function getCollocation()
+    {
+
+        return $this->collocation;
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require_once __DIR__ . '/models/products/Product.php';
 require_once __DIR__ . '/models/products/Book.php';
 require_once __DIR__ . '/models/products/Game.php';
+require_once __DIR__ . '/User.php';
 
 
 $item = new Product('Lor of the Rings', 'Cashier', 20, 'Accesory');
@@ -9,9 +10,11 @@ $book = new Book('Lord of the Rings', 'Fantasy', ' J.R.Tolkien', 'Fantasy', '452
 
 $callofDuty = new Game('Call of Duty', 'Shooter - Section', 'Activision', 'Shooter', 60, 'Ps4');
 
-var_dump($item);
-var_dump($callofDuty);
-var_dump($book);
+
+// var_dump($item);
+// var_dump($callofDuty);
+// var_dump($book);
+// var_dump($user1);
 ?>
 
 
@@ -26,6 +29,16 @@ var_dump($book);
 </head>
 
 <body>
+
+    <section>
+        <?php $user1 = new User('Michau', 'Wadowski', 'micio@miao.it'); ?>
+
+        <h1> <?= $user1->getUserName() ?></h1>
+        <h3><?= $user1->getDiscount() ?></h3>
+        <div>
+
+        </div>
+    </section>
 
 </body>
 
